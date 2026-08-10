9dc83dcb5324103c263eaab360faf4f671170bd7
Django <behruzbekb98@gmail.com>
2026-08-10 16:10:03 +0500

    add

--- blog_app/models.py
+++ blog_app/models.py
@@ -1,3 +1,44 @@
+from django.urls import reverse
+from django.utils import timezone
+
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class PublishManager(models.Manager):
+    def get_queryset(self):
+        return super(PublishManager,self).get_queryset().filter(status='publish')
+
+
+
+class Post(models.Model):
+    STATUS_CHOICES = (
+        ('draft','Draft'),
+        ('published','Published'),
+    )
+
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200, unique_for_date='publish')
+    author = models.ForeignKey(User, on_delete=models.CASCADE,related_name='blog_post')
+    body = models.TextField()
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=100,choices=STATUS_CHOICES,default='draft')
+
+    class Meta:
+        ordering = ('-publish',)
+
+    def __str__(self):
+        return self.title
+
+    objects = models.Manager()
+    published = PublishManager()
+
+
+    def get_absolute_url(self):
+        return reverse("blog:post_detail", args=[self.publish.year, self.publish.month, self.publish.day, self.slug])
+
+post = Post.objects.all()
+p_post = Post.published.all()
